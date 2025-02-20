809c12f9f043b87b1ae3078b86a677d21527e0fb
Shalom-227 <alaladeshalom02@gmail.com>
2025-02-20 17:11:19 +0000

    inserted sample library name and sample librarian name

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -8,10 +8,10 @@ author = Author.objects.get(name = "")
 print(author.book_set.all())
 
 #List all books in a library.
-library = Library.objects.get(name="library_name")
+library = Library.objects.get(name=library_name)
 print(library.books.all())
 
 #Retrieve the librarian for a library.
-librarian = Librarian.objects.get(name="librarian_name")
+librarian = Librarian.objects.get(name=librarian_name)
 print(librarian.library)
 
