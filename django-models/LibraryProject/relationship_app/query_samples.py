374be75829f0a79b7312b3f0a2571c64ee019cee
Shalom-227 <alaladeshalom02@gmail.com>
2025-02-20 17:16:42 +0000

    corrected code to match checker

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,8 +4,8 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 #Query all books by a specific author.
 
-author = Author.objects.get(name = "")
-print(author.book_set.all())
+author = Author.objects.get(name = author_name)
+print(Book.objects.filter(author=author)
 
 #List all books in a library.
 library = Library.objects.get(name=library_name)
