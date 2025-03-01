3d3d2372b0e81c23e7eb37deed78e72c92a047ae
Shalom-227 <alaladeshalom02@gmail.com>
2025-03-01 17:39:59 +0000

    fixing code

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -4,4 +4,9 @@ from .models import Article
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['title', 'author', 'content']
+        fields = ['title', 'author', 'content']i
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
