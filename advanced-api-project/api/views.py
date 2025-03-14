f53993db406802dc5d731514fa60706afcbef34f
Shalom-227 <alaladeshalom02@gmail.com>
2025-03-14 14:00:36 +0000

    fixed code to match checker

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework import permissions 
+from rest_framework import permissions
 from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
 from datetime import datetime
