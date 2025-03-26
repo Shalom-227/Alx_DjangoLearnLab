b564dc3f5ad0ebb9cfde290db60937b448a566fb
Shalom-227 <alaladeshalom02@gmail.com>
2025-03-26 22:41:18 +0000

    adjusted RegistrationSerializer and LoginSerializer to match checker requirement

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, get_user_model
 from rest_framework.authtoken.models import Token
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
     password2 = serializers.CharField(write_only=True)  # Confirm password
     
     class Meta:
