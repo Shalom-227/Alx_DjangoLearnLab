b28c609997c4ec797ac8439d43e687af7086e6b1
Shalom-227 <alaladeshalom02@gmail.com>
2025-03-26 21:42:50 +0000

    changed field type in bio field of the CustomUser to Textfield

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -6,7 +6,7 @@ from django.core.validators import MinLengthValidator
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    bio = models.CharField(validators=[MinLengthValidator(5)], max_length = 150, help_text= "Tell people something about yourself")
+    bio = models.TextField(validators=[MinLengthValidator(5)], max_length = 150, help_text= "Tell people something about yourself")
     profile_picture = models.ImageField(upload_to="profile_pictures", null=True, blank=True)
     followers = models.ManyToManyField("self", symmetrical=False)
 
